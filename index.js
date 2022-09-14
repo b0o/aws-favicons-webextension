ec8c7e878cf940a5002e45dffb130834afdbb7b0
Maddison Hellstrom <maddy@na.ai>
2022-09-13 18:07:49 -0700

    Format index.js with prettier

--- index.js
+++ index.js
@@ -1,60 +1,56 @@
-const awsBaseURL = (u) => {
+export const awsBaseURL = (u) => {
   if (typeof u === "string" && !u.match(/^http(s)?:\/\//)) {
-    const _u = new URL("https://console.aws.amazon.com");
-    _u.pathname = u;
-    u = _u;
+    const _u = new URL("https://console.aws.amazon.com")
+    _u.pathname = u
+    u = _u
   }
-  const url = u instanceof URL ? u : new URL(u);
-  url.search = "";
-  url.hash = "";
-  url.pathname = url.pathname.split("/").at(1) ?? "";
+  const url = u instanceof URL ? u : new URL(u)
+  url.search = ""
+  url.hash = ""
+  url.pathname = url.pathname.split("/").at(1) ?? ""
   url.hostname = url.hostname.replace(
     /[^.]+\.(console\.aws\.amazon\.com)$/,
     "$1"
-  );
-  return `${url.hostname}${url.pathname
-    .replace(/\/+/, "/")
-    .replace(/\/$/, "")}`;
-};
+  )
+  return `${url.hostname}${url.pathname.replace(/\/+/, "/").replace(/\/$/, "")}`
+}
 
 const getAwsServices = async () => {
-  const url = chrome.runtime.getURL("/services.json");
-  return (await fetch(url)).json();
-};
+  const url = chrome.runtime.getURL("/services.json")
+  return (await fetch(url)).json()
+}
 
 const getAwsServiceForURL = (services, url) => {
-  const bu = awsBaseURL(url);
-  return services[bu];
-};
+  const bu = awsBaseURL(url)
+  return services[bu]
+}
 
 const getLocalFaviconURL = (service) =>
-  chrome.runtime.getURL(`/icons/${service.id}.svg`);
+  chrome.runtime.getURL(`/icons/${service.id}.svg`)
 
 const setFavicon = async (href) => {
-  let link = document.querySelector("link[rel=icon]");
+  let link = document.querySelector("link[rel=icon]")
   if (link) {
     if (link.href === href) {
-      return;
+      return
     }
   } else {
-    link = document.createElement("link");
-    document.head.appendChild(link);
+    link = document.createElement("link")
+    document.head.appendChild(link)
   }
-  link.type = "image/svg+xml";
-  link.rel = "icon";
-  link.href = href;
-};
+  link.type = "image/svg+xml"
+  link.rel = "icon"
+  link.href = href
+}
 
 const main = async () => {
-  const services = await getAwsServices();
-  const service = getAwsServiceForURL(services, window.location);
+  const services = await getAwsServices()
+  const service = getAwsServiceForURL(services, window.location)
   if (service) {
-    await setFavicon(getLocalFaviconURL(service));
+    await setFavicon(getLocalFaviconURL(service))
   }
-};
+}
 
-if (typeof module === "object" && module.exports) {
-  module.exports.awsBaseURL = awsBaseURL;
-} else {
-  main();
+if (typeof browser !== "undefined") {
+  main()
 }
